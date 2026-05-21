93bdf7c656118a1d34487e3edbef58ddfbc7430c
Prateeksha Pandey <prateekshapandey643@gmail.com>
2026-05-21 11:06:17 +0530

    fixed base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/MyPortfolio/',
   server: {
     open: true,
     port: 5010,
